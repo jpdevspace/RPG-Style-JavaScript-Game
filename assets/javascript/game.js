aa04e6bac73515363a619ba334ed54639a31c295
jpwebdev <jjpwebdev@gmail.com>
2017-12-07 22:01:31 -0500

    basic layout: 3 sections to play, character cards created.

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+// Set all variables and inital values
+
+//1. Listen for click events from user to choose character
+    // Set  userChoice = true
+    // if userChoice ? move to attack zone : move to enemy zone
+//2. Choose your opponent 
+    // onclick character.currentEnemy = true
+    // oponentExists = true
+    // move it to the attacking zone
+//3. Attack your oponent by clicking Attack button
+    // function attack() {
+    // if oponentExists {
+        // increase users attack power (even if it's the 1st round it will work bc attack power = 0 on round 1)
+        // reduce enemies health
+        // if enemies health > 0 ? fire getHit() : increase subwin counter
+        // if subwin counter == (charactersArr.length)-1 (you) ? win entire game and show restart button : getHit()
+        // }
+    //}else{display no oponents}
+//4. Get hit
+    // function getHit(){
+    // reduce users health
+    // if user's health == 0 ? game over : attackOpenent()
+    //}
